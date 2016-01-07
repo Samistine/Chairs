a0351a2599e52ecf0d1882e010707416617a3a1b
Samistine <samuel@samistine.com>
2016-01-07 17:48:24 -0500

    Refractor
    I decided to do this in a new commit vs the last

--- src/com/cnaude/chairs/sitaddons/ChairEffects.java
+++ src/com/cnaude/chairs/sitaddons/ChairEffects.java
@@ -72,16 +72,16 @@ public class ChairEffects {
         @Override
         public void run() {
             for (Player p : plugin.getPlayerSitData().getSittingPlayers()) {
-                    if (p.hasPermission("chairs.sit.health")) {
-                        double pHealthPcnt = (getPlayerHealth(p)) / getMaxPlayerHealth(p) * 100d;
-                        if ((pHealthPcnt < plugin.getConfigData().getSitMaxHealth()) && (getPlayerHealth(p) < getMaxPlayerHealth(p))) {
-                            double newHealth = plugin.getConfigData().getSitHealthPerInterval() + getPlayerHealth(p);
-                            if (newHealth > getMaxPlayerHealth(p)) {
-                                newHealth = getMaxPlayerHealth(p);
-                            }
-                            p.setHealth(newHealth);
+                if (p.hasPermission("chairs.sit.health")) {
+                    double pHealthPcnt = (getPlayerHealth(p)) / getMaxPlayerHealth(p) * 100d;
+                    if ((pHealthPcnt < plugin.getConfigData().getSitMaxHealth()) && (getPlayerHealth(p) < getMaxPlayerHealth(p))) {
+                        double newHealth = plugin.getConfigData().getSitHealthPerInterval() + getPlayerHealth(p);
+                        if (newHealth > getMaxPlayerHealth(p)) {
+                            newHealth = getMaxPlayerHealth(p);
                         }
+                        p.setHealth(newHealth);
                     }
+                }
             }
         }
 
@@ -99,46 +99,46 @@ public class ChairEffects {
         @Override
         public void run() {
             for (Player p : plugin.getPlayerSitData().getSittingPlayers()) {
-                    for (Entity entity : p.getNearbyEntities(1, 2, 1)) {
-                        if (entity instanceof Item) {
-                            Item item = (Item) entity;
-                            if (item.getPickupDelay() == 0) {
-                                if (p.getInventory().firstEmpty() != -1) {
-                                    PlayerPickupItemEvent pickupevent = new PlayerPickupItemEvent(p, item, 0);
-                                    plugin.getServer().getPluginManager().callEvent(pickupevent);
-                                    if (!pickupevent.isCancelled()) {
-                                        p.getInventory().addItem(item.getItemStack());
-                                        entity.remove();
-                                    }
+                for (Entity entity : p.getNearbyEntities(1, 2, 1)) {
+                    if (entity instanceof Item) {
+                        Item item = (Item) entity;
+                        if (item.getPickupDelay() == 0) {
+                            if (p.getInventory().firstEmpty() != -1) {
+                                PlayerPickupItemEvent pickupevent = new PlayerPickupItemEvent(p, item, 0);
+                                plugin.getServer().getPluginManager().callEvent(pickupevent);
+                                if (!pickupevent.isCancelled()) {
+                                    p.getInventory().addItem(item.getItemStack());
+                                    entity.remove();
                                 }
                             }
-                        } else if (entity instanceof ExperienceOrb) {
-                            ExperienceOrb eorb = (ExperienceOrb) entity;
-                            int exptoadd = eorb.getExperience();
-                            while (exptoadd > 0) {
-                                int localexptoadd = 0;
-                                if (p.getExpToLevel() < exptoadd) {
-                                    localexptoadd = p.getExpToLevel();
-                                    PlayerExpChangeEvent expchangeevent = new PlayerExpChangeEvent(p, localexptoadd);
-                                    plugin.getServer().getPluginManager().callEvent(expchangeevent);
-                                    p.giveExp(expchangeevent.getAmount());
-                                    if (p.getExpToLevel() <= 0) {
-                                        PlayerLevelChangeEvent levelchangeevent = new PlayerLevelChangeEvent(p, p.getLevel(), p.getLevel() + 1);
-                                        plugin.getServer().getPluginManager().callEvent(levelchangeevent);
-                                        p.setExp(0);
-                                        p.giveExpLevels(1);
-                                    }
-                                } else {
-                                    localexptoadd = exptoadd;
-                                    PlayerExpChangeEvent expchangeevent = new PlayerExpChangeEvent(p, localexptoadd);
-                                    plugin.getServer().getPluginManager().callEvent(expchangeevent);
-                                    p.giveExp(expchangeevent.getAmount());
-                                }
-                                exptoadd -= localexptoadd;
-                            }
-                            entity.remove();
                         }
+                    } else if (entity instanceof ExperienceOrb) {
+                        ExperienceOrb eorb = (ExperienceOrb) entity;
+                        int exptoadd = eorb.getExperience();
+                        while (exptoadd > 0) {
+                            int localexptoadd = 0;
+                            if (p.getExpToLevel() < exptoadd) {
+                                localexptoadd = p.getExpToLevel();
+                                PlayerExpChangeEvent expchangeevent = new PlayerExpChangeEvent(p, localexptoadd);
+                                plugin.getServer().getPluginManager().callEvent(expchangeevent);
+                                p.giveExp(expchangeevent.getAmount());
+                                if (p.getExpToLevel() <= 0) {
+                                    PlayerLevelChangeEvent levelchangeevent = new PlayerLevelChangeEvent(p, p.getLevel(), p.getLevel() + 1);
+                                    plugin.getServer().getPluginManager().callEvent(levelchangeevent);
+                                    p.setExp(0);
+                                    p.giveExpLevels(1);
+                                }
+                            } else {
+                                localexptoadd = exptoadd;
+                                PlayerExpChangeEvent expchangeevent = new PlayerExpChangeEvent(p, localexptoadd);
+                                plugin.getServer().getPluginManager().callEvent(expchangeevent);
+                                p.giveExp(expchangeevent.getAmount());
+                            }
+                            exptoadd -= localexptoadd;
+                        }
+                        entity.remove();
                     }
+                }
             }
         }
     }
